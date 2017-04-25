ddd81f22514b3bc33f029b1ba9fc0cf3f587b935
rendaniluk <lukhwa11582924>
2017-04-25 10:43:08 +0200

    investment calculator

---
+++ app.js
@@ -0,0 +1,17 @@
+//the Dom
+var initialDeposit = document.querySelector('.P');
+var annualInterest = document.querySelector('.r');
+var numberOfyears = document.querySelector('.t');
+var interestTimes = document.querySelector('.n');
+var calculate = document.querySelector('.btn');
+var outPut = document.querySelector('.outPut')
+//future value of an investment
+var myCalculator = function() {
+  var initialDepositValue = initialDeposit.value;
+  var annualInterestValue = (annualInterest.value)/100;
+  var numberOfyearsValue = numberOfyears.value;
+  var interestTimesValue = interestTimes.value;
+  A = initialDepositValue * Math.pow(1 + (annualInterestValue/interestTimesValue), (numberOfyearsValue*interestTimesValue));
+  outPut.innerHTML = 'R' + " " + A.toFixed(2);
+};
+calculate.addEventListener('click', myCalculator);
